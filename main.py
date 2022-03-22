86911a4fdb7d78a51f6ca2927b5413e77eaf1ea2
Marcos Sousa <a.marcos.car@bol.com.br>
2022-03-22 10:32:38 -0300

    Adicionado função da segunda parte do turrial

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
 
 """
 app = FastAPI()
@@ -25,10 +26,53 @@ def update_item(item_id: int, item: Item):
     return {"item_price": item.price, "item_id": item_id}
 """
 
-#Codigos do Primeiros Passos
+# Codigos do Guia 'Primeiros Passos'
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello Wold"}
+
+
+@app.get("/items/{item_id}")
+# async def read_tem(item_id):      # Pode ser usado qualquer valor, int, float strings
+async def read_item(item_id: int):   # Somente tipos int "Validação de dados"
+    return {"item_id": item_id}
+
+
+@app.get("/users/me")                       # Ver usuario atual
+async def read_user_me():
+    return {"usuer_id:" "the current user"}
+
+
+@app.get("/user/{user_id}")
+async def read_user(user_id: str): # Permitir entrar com um usuario
+    return {"user_id": user_id}
+
+# Usa Enum para criar classes que herdam str de Enum
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+# Enum é biblioteca padrão python > 3.5
+@app.get("/models/{model_name}") # Cria classes que herdam str de Enum
+async def get_model(model_name: ModelName): # Paramentro de caminho
+    if model_name == ModelName.alexnet: # Compara membros de enumeração
+        return {"model_name" : model_name, "message" : "Deep Learning FTW"} # Retorna menbros de Enum
+    
+    if model_name.value == "lenet": # Obtem o valor de Enum
+        return {"model_name": model_name, "message": "LeCNN all the images"} # Retorna menbros de Enum
+    
+    return {"model_name": model_name, "message": "Have some residuals"} # Retorna menbros de Enum
+
+# Usando conversor de caminhos em 'PATH'
+# Usa função direta do starlette
+@app.get("/files/{file_path:path}") 
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+
+
