4cffbe58af45286766e5da38ccad932cf5e7f5cd
Marcos Sousa <a.marcos.car@bol.com.br>
2022-03-22 09:03:31 -0300

    Fim do primeiro tutorial e incio do Guia Primeiros passos

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# Codigo do primeiro tutorial
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+"""
 app = FastAPI()
 
 class Item(BaseModel):
@@ -20,4 +22,13 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {"item_price": item.price, "item_id": item_id}
+"""
+
+#Codigos do Primeiros Passos
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Wold"}
